da965078a3266fb6245cce3f1a9115f1dd49a02c
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2025-08-21 03:39:44 -0400

    Update inject.cpp

--- inject.cpp
+++ inject.cpp
@@ -4,35 +4,46 @@
 #include <cstdio>  
 
 int main(int argc, char *argv[]) { 
-        if (argc == 1 || argv[1] == "--help") { 
-                std::cout << "INJECT \n\n" << "Usage: inject [File] [Line Number] [Content] \n";
-                return -3; // Help Command 
-        } 
-        else if (argc < 4) { 
-                std::cout << "Invalid, Not Enough Arguments; Run --help For More Info. \n"; 
-                return -2; // Not Enough Args 
-        } 
-        std::ifstream file(argv[1]);
+	if (argc == 1 || argv[1] == "--help") { 
+		std::cout << "INJECT \n\n" << "Usage: inject [File] [Line Number] [Content] \n";
+		return -3; // Help Command 
+	} 
+	else if (argc < 4) { 
+		std::cout << "Invalid, Not Enough Arguments; Run --help For More Info. \n"; 
+		return -2; // Not Enough Args 
+	} 
+	std::ifstream file(argv[1]);
         std::string* pHemp = new std::string("temp_"); 
-        *pHemp+=argv[1]; 
-        const char* THEMP = (*pHemp).c_str(); 
-        delete pHemp; 
-        std::ofstream tfile(THEMP); 
-        long num = std::stoi(argv[2]); 
-        std::string line; 
-        long li = 0; 
-        while (std::getline(file, line)) { 
-                if (li == num) { 
-                        for (short i=3;i<argc;i++) { tfile << argv[i]; tfile << " "; } 
-                } 
-                else { 
-                        tfile << line; 
-                } 
-                li++; 
-        } 
-        file.close(); 
-        tfile.close(); 
-        std::remove(argv[1]); 
-        std::rename(THEMP, argv[1]); 
-        return 0; 
+	*pHemp+=argv[1]; 
+	const char* THEMP = (*pHemp).c_str(); 
+	delete pHemp; 
+	std::ofstream tfile(THEMP); 
+	long num = std::stoi(argv[2]); 
+	std::string line; 
+	long li = 0; 
+	bool numEx = false; 
+	while (std::getline(file, line)) { 
+		if (li == num) { 
+		        for (short i=3;i<argc;i++) { tfile << argv[i]; } tfile << " \n"; numEx=1;  
+		} 
+		else { 
+			tfile << line; tfile << " \n";  
+		} 
+		li++; 
+	} 
+	if (!numEx) { 
+		if(li<1) { tfile << " \n"; }  
+		for (short j=3;j<argc;j++) { 
+			tfile << argv[j]; 
+		} 
+		tfile << " \n"; 
+	}  
+	if (file.good()) { 
+		std::remove(argv[1]); 
+	}
+	file.close(); 
+	tfile.close();  
+	std::rename(THEMP, argv[1]); 
+	return 0; 
 } 
+
